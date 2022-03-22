307440c53447a215831297f1182b4d13603e62af
Balu Shrama <69688211+Balanjaneyasharma@users.noreply.github.com>
2022-03-22 18:26:32 +0530

    Create GCDProblemGoalState.cpp

---
+++ GCDProblemGoalState.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int gcd(int x,int y){
+    if(x==0) return y;
+    if(y==0) return x;
+    if(x<y) return gcd(x,y%x);
+    else return gcd(x%y,y);
+}
+
+int main()
+{
+    int x,y;
+    cin>>x>>y;
+    if(gcd(x,y)==1){
+        cout<<"yes"<<endl;
+    }
+    else cout<<"no"<<endl;
+    return 0;
+}
